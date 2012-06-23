acc1ab7d43ec519a49cb1c9ef69a66e1f1bcb510
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-06-22 22:22:06 -0400

    Added contact page, installed libs for guard and spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,47 +3,35 @@ require 'spec_helper'
 describe "StaticPages" do
   
   describe "Home page" do
-    
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page_has("home", "Sample App")
     end
   end
   
   describe "Help page" do
-  
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      page_has("help", "Help")
     end
   end
   
   describe "About page" do
   
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      page_has("about", "About Us")
     end
   end
   
-  describe "Title test" do
-    
-    it "should have the  title 'Home'" do
-        visit '/static_pages/home'
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-    end
-    
-    it "should have the  title 'Help'" do
-        visit '/static_pages/help'
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
-    
-    it "should have the  title 'About Us'" do
-        visit '/static_pages/about'
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      page_has("contact", "Contact Us")
     end
   end
-  
+
 end
 
 
+def page_has(page_name, description)
+     visit "/static_pages/#{page_name}"
+     page.should have_selector('h1', :text => description)
+     page.should have_selector('title', :text => description)
+end
