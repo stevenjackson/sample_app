f7aca1ce1c4ed292a779cde9ec893dce45cef612
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-06-23 17:40:58 -0400

    Route static pages to base paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,39 +4,39 @@ describe "StaticPages" do
   
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      page_has("home", "Sample App")
+      page_has(root_path, "Sample App")
     end
     
     it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
+      visit root_path
+      page.should_not have_selector('title', text: '| Home')
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      page_has("help", "Help")
+      page_has(help_path, "Help")
     end
   end
   
   describe "About page" do
   
     it "should have the content 'About Us'" do
-      page_has("about", "About Us")
+      page_has(about_path, "About Us")
     end
   end
   
   describe "Contact page" do
     it "should have the content 'Contact Us'" do
-      page_has("contact", "Contact Us")
+      page_has(contact_path, "Contact Us")
     end
   end
 
 end
 
 
-def page_has(page_name, description)
-     visit "/static_pages/#{page_name}"
-     page.should have_selector('h1', :text => description)
-     page.should have_selector('title', :text => description)
+def page_has(page_path, description)
+     visit page_path
+     page.should have_selector('h1', text: description)
+     page.should have_selector('title', text: description)
 end
