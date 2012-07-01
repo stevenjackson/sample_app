563f6e1a5c82f79a0136ff5a333ee962c69fcbf3
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-07-01 15:21:44 -0400

    Add user GET page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,12 @@ describe "User pages" do
   	it { should have_selector('h1', text: 'Sign up') }
   	it { should have_selector('title', text: 'Sign up') }
   end
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
 end
