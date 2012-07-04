a74200255577f6feccfa0bc23b810d5c94e555b3
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-07-04 14:27:25 -0400

    Added user profile edit page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,7 +39,7 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -55,6 +55,21 @@ describe "User pages" do
         it { should have_link('Sign out') }
       end
     end
-    
   end
+  
+  describe "User pages" do
+  	describe "edit" do
+  		let(:user) { FactoryGirl.create(:user) }
+  		before { visit edit_user_path(user) }
+  		
+  		describe "page" do
+  			it { should have_selector('h1',    text: "Update your profile") }
+      	it { should have_selector('title', text: "Edit user") }
+      	it { should have_link('change', href: 'http://gravatar.com/emails') }
+  		end
+  		
+  		
+  	end
+  end
+  
 end
