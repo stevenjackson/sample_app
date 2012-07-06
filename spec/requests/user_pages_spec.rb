c63bff82755128860f903e5acda8ec9b39c80f04
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-07-05 20:07:43 -0400

    Add pagination to users page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -115,6 +115,20 @@ describe "User pages" do
       end
     end
     
+    describe "pagination" do
+  
+    	before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+    	
+    	it { should have_selector('div.pagination') }
+    	
+    	it "should list each user" do
+    	  User.paginate(page: 1).each do |user|
+    	  end
+    	end
+    	
+    end
+    
   end
   
 end
