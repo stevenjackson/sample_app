e9c05a591eb3615d96d6078a91b658f7826d4825
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-07-07 16:43:21 -0400

    Add micropost creation to home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,6 +3,13 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "published successfully"
+      redirect_to root_path
+    else
+      render 'static_pages/home'
+    end
   end
   
   def destroy
