777da3311479b31e5fbcb585182e1720e9aae39d
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-07-08 18:04:38 -0400

    Add tests for non-signed in user

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -141,8 +141,24 @@ describe "authorization" do
       specify { response.should redirect_to(signin_path) }
     end
     
-    describe "using create action" do
+    describe "using delete action" do
       before { delete micropost_path(FactoryGirl.create(:micropost)) }
+      specify { response.should redirect_to(signin_path) }
+    end
+  
+  end
+  
+  describe "require sign in when modifying relationships" do
+  
+    let(:user) { FactoryGirl.create(:user) }
+
+    describe "using create action" do
+      before { post relationships_path }
+      specify { response.should redirect_to(signin_path) }
+    end
+    
+    describe "using delete action" do
+      before { delete relationship_path(1) }
       specify { response.should redirect_to(signin_path) }
     end
   
