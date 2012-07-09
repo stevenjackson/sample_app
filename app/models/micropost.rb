197d779f9fbe548ab4db242311b01f6d11e298a9
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-07-08 21:53:18 -0400

    Add followed users to feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,9 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   
   default_scope order: 'microposts.created_at DESC'
+  
+  def Micropost.from_users_followed_by(user)
+    followed_ids = user.followed_user_ids
+    Micropost.where("user_id in (?) or user_id = ?", followed_ids, user)
+  end
 end
